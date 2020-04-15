86d3613fc97a778ef72a11d0566547fbf164f112
Alex Boisseau <alex.boisseau@ynov.com>
2020-04-15 22:01:55 +0200

    installation de l'API plateform et mise en place de nos ressources

---
+++ src/Controller/InvoiceIncrementationController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Invoice;
+use Doctrine\ORM\EntityManagerInterface;
+
+class InvoiceIncrementationController {
+    
+    /** 
+     * @var EntityManagerInterface
+     */
+    private $manager;
+    
+    public function __construct(EntityManagerInterface $manager){
+        $this->manager = $manager;
+    }
+    
+    /**
+     * Undocumented function
+     *
+     * @param Invoice $data
+     * @return void
+     */
+    public function __invoke(Invoice $data)
+    {
+        $data->setChrono($data->getChrono() + 1);
+
+        $this->manager->flush();
+
+        return $data;
+    }
+}
